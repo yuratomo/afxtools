879be86f589eb1463dc9793e4243d3b7379f8057
tomoharu saito <yura.tomo@gmail.com>
2013-10-11 00:59:54 +0900

    afxtaskを追加

---
+++ afxtask/afxtask.cpp
@@ -0,0 +1,419 @@
+/**
+ * あふwのファイル窓にプロセス一覧を表示するプラグイン
+ * 
+ */
+#include <string.h>
+#include <stdio.h>
+#include "afxwapi.h"
+#include "afxcom.h"
+#include "version.h"
+#include <windows.h>
+#include <tlhelp32.h>
+#include <wchar.h>
+#include <Psapi.h>      // Psapi.Lib
+
+#define PLUGIN_NAME    L"afxtask"
+#define PLUGIN_AUTHOR  L"yuratomo"
+
+typedef struct _PluginData {
+	AfxwInfo afx;
+	HANDLE hSnapshot;
+} PluginData, *lpPluginData;
+
+int _pid2item(PROCESSENTRY32* entry, lpApiItemInfo lpItemInfo);
+IDispatch* pAfxApp = NULL;
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason,LPVOID lpvReserved)
+{
+    switch(fdwReason)
+    {
+        case DLL_PROCESS_ATTACH:
+			AfxInit(pAfxApp);
+            break;
+
+        case DLL_PROCESS_DETACH:
+			AfxCleanup(pAfxApp);
+            break;
+
+        case DLL_THREAD_ATTACH:
+            break;
+
+        case DLL_THREAD_DETACH:
+            break;
+    }
+    return  TRUE;
+}
+
+/**
+ * プラグイン情報を取得する。
+ * @param[out] pluginInfo    プラグイン情報。
+ */
+void WINAPI ApiGetPluginInfo(lpApiInfo pluginInfo)
+{
+	if (pluginInfo == NULL) {
+		return;
+	}
+
+	memset(pluginInfo, 0, sizeof(ApiInfo));
+
+	// プラグイン名
+	wcsncpy(pluginInfo->szPluginName, PLUGIN_NAME, API_MAX_PLUGIN_NAME_LENGTH-1);
+
+	// 作者
+	wcsncpy(pluginInfo->szAuthorName, PLUGIN_AUTHOR, API_MAX_AUTHOR_NAME_LENGTH-1);
+
+	// プラグインバージョン
+	pluginInfo->dwVersion = MAKE_VERSION(VERSION_MAJOR, VERSION_MINOR, VERSION_BUGFIX, VERSION_REVISION);
+}
+
+/**
+ * プラグインオープンする。
+ * @param[in]  szCommandLine プラグイン側に渡すコマンドライン。
+ * @param[in]  afxwInfo      あふwの情報。
+ * @param[out] openInfo      オープン情報。
+ * @retval     0以外         プラグインで自由に使えるハンドル。その他APIのhandleとして使用する。
+ * @retval     NULL(0)       オープン失敗。
+ */
+HAFX WINAPI ApiOpen(LPCWSTR szCommandLine, const lpAfxwInfo afxwInfo, lpApiOpenInfo openInfo)
+{
+	lpPluginData pdata = (lpPluginData)GlobalAlloc(GMEM_FIXED, sizeof(PluginData));
+	if (pdata == NULL) {
+		return 0;
+	}
+
+	memset(pdata, 0, sizeof(PluginData));
+	memcpy(&pdata->afx, afxwInfo, sizeof(AfxwInfo));
+
+	wcscpy(openInfo->szBaseDir, L"");
+	wcscpy(openInfo->szInitRelDir, L"");
+	wcsncpy(openInfo->szTitle, szCommandLine, sizeof(openInfo->szTitle));
+
+	pdata->hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (pdata->hSnapshot == INVALID_HANDLE_VALUE) {
+		return 0;
+	}
+
+	return (HAFX)pdata;
+}
+
+/**
+ * プラグインクローズする。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiClose(HAFX handle)
+{
+	lpPluginData pdata = (lpPluginData)handle;
+	if (pdata == NULL) {
+		return 0;
+	}
+
+	CloseHandle(pdata->hSnapshot);
+
+	if (pdata != NULL) {
+		GlobalFree(pdata);
+	}
+	return 1;
+}
+
+/**
+ * szDirPathで指定された仮想ディレクトリパスの先頭アイテムを取得する。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szDirPath     取得したいディレクトリフルパス。
+ * @param[out] lpItemInfo    先頭アイテムの情報。
+ * @retval     1             アイテムを見つけた場合
+ * @retval     0             エラー
+ * @retval     -1            検索終了
+ */
+int  WINAPI ApiFindFirst(HAFX handle, LPCWSTR szDirPath, /*LPCWSTR szWildName,*/ lpApiItemInfo lpItemInfo)
+{
+	lpPluginData pdata = (lpPluginData)handle;
+	if (pdata == NULL) {
+		return 0;
+	}
+	if (lpItemInfo == NULL) {
+		return -1;
+	}
+
+	PROCESSENTRY32 entry;
+	entry.dwSize = sizeof(PROCESSENTRY32);
+	
+	if (!Process32First(pdata->hSnapshot, &entry)) {
+		return -1;
+	}
+
+	return _pid2item(&entry, lpItemInfo);
+}
+
+/**
+ * ApiFindFirstで取得したアイテム以降のアイテムを取得する。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[out] lpItemInfo    先頭アイテムの情報。
+ * @retval     1             アイテムを見つけた場合
+ * @retval     0             エラー
+ * @retval     -1            検索終了
+ */
+int  WINAPI ApiFindNext(HAFX handle, lpApiItemInfo lpItemInfo)
+{
+	lpPluginData pdata = (lpPluginData)handle;
+	if (pdata == NULL) {
+		return 0;
+	}
+	if (lpItemInfo == NULL) {
+		return -1;
+	}
+
+	PROCESSENTRY32 entry;
+	entry.dwSize = sizeof(PROCESSENTRY32);
+
+	if (!Process32Next(pdata->hSnapshot, &entry)){
+		return -1;
+	}
+
+	return _pid2item(&entry, lpItemInfo);
+}
+
+int _pid2item(PROCESSENTRY32* entry, lpApiItemInfo lpItemInfo)
+{
+	// アイテム名
+	swprintf(lpItemInfo->szItemName, L"%d %s", entry->th32ProcessID, entry->szExeFile);
+	lpItemInfo->dwAttr = FILE_ATTRIBUTE_NORMAL;
+
+	// メモリ使用量や開始時刻を調べるためにいったんOpenする
+	HANDLE hProc = OpenProcess(PROCESS_QUERY_INFORMATION|PROCESS_VM_READ, FALSE, entry->th32ProcessID);
+	if (hProc == NULL) {
+		// エラーにはしない
+		lpItemInfo->ullItemSize = 0L;
+		return 1;
+	}
+
+	// メモリ使用量
+	PROCESS_MEMORY_COUNTERS pmc = { 0 };
+	if (GetProcessMemoryInfo(hProc,&pmc,sizeof(pmc))) {
+		lpItemInfo->ullItemSize = pmc.PeakWorkingSetSize;
+	} else {
+		lpItemInfo->ullItemSize = 0L;
+	}
+
+	// プロセスの開始時刻
+	FILETIME creationTime;//プロセスの作成時刻
+	FILETIME exitTime;//プロセスの終了時刻
+	FILETIME kernelTime;//カーネルモードでのプロセス動作時間
+	FILETIME userTime;//ユーザーモードでのプロセス動作時間
+	GetProcessTimes(hProc, 
+		&creationTime, 
+		&exitTime, 
+		&kernelTime, 
+		&userTime);
+	lpItemInfo->ullTimestamp = creationTime;
+
+	// ここで閉じる
+	CloseHandle( hProc );
+
+	return 1;
+}
+
+/**
+ * アイテムを拡張子判別実行する。
+ * あふwでENTERやSHIFT-ENTERを押したときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szItemPath    アイテムのフルパス。
+ * @retval     2             あふw側に処理を任せる。（ApiCopyでテンポラリにコピーしてから実行)
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiExecute(HAFX handle, LPCWSTR szItemPath)
+{
+	return 0;
+}
+
+/**
+ * アイテムをコピーする。
+ * あふwでコピー処理をするときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szFromItem    コピー元のアイテム名。
+ * @param[in]  szToPath      コピー先のフォルダ。
+ * @param[in]  lpPrgRoutine  コールバック関数(CopyFileExと同様)
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiCopyTo(HAFX handle, LPCWSTR szFromItem, LPCWSTR szToPath, LPPROGRESS_ROUTINE lpPrgRoutine)
+{
+	return 0;
+}
+
+/**
+ * @brief あふwが内部的に利用するコピー処理。
+ * 主にプラグイン内のファイルを開く場合に、AFXWTMP以下にコピーするために使われる。
+ * ただし、実際にコピーするかどうかはプラグイン次第で、あふwはszOutputPathで指定されたファイルを開く。
+ *
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szFromItem    プラグイン内のアイテム名
+ * @param[in]  szToPath      コピー先のフォルダ(c:\Program Files\AFXWTMP.0\hoge.txt等が指定される)
+ * @param[out] szOutputPath  プラグインが実際にコピーしたファイルパス
+ *                           szToPathにコピーする場合はszToPathをszOutputPathにコピーしてあふwに返す。
+ *                           szToPathにコピーしない場合、あふwに開いてほしいファイルのパスを指定する。
+ * @param[in]  dwOutPathSize szOutputPathのバッファサイズ
+ * @param[in]  lpPrgRoutine  コールバック関数(CopyFileExと同様)
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiIntCopyTo(HAFX handle, LPCWSTR szFromItem, LPCWSTR szToPath, LPWSTR szOutputPath, DWORD dwOutPathSize, LPPROGRESS_ROUTINE lpPrgRoutine)
+{
+	int ProcessID;
+	wchar_t buf[1024];
+	wchar_t path[MAX_PATH];
+	swscanf(szFromItem, L"%d %s", &ProcessID, buf);
+
+	HANDLE hProc = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, ProcessID);
+	if (hProc == NULL) {
+		return 0;
+	}
+
+	swprintf(path, L"%s%s", szToPath, PLUGIN_NAME);
+
+	FILE* fp = _wfopen(path, L"w");
+	if (fp == NULL) {
+		return 1;
+	}
+
+	HMODULE hModule = NULL;
+	DWORD dummy = 0;
+	if(!EnumProcessModules(hProc, &hModule, sizeof(HMODULE), &dummy)) {
+		GetModuleFileNameEx(hProc, hModule, buf, sizeof(buf));
+	}
+	fwprintf(fp, L"Path: %s\n", buf);
+
+	fclose(fp);
+
+	CloseHandle( hProc );
+	wcsncpy(szOutputPath, path, dwOutPathSize - 1);
+
+	return 0;
+}
+
+/**
+ * アイテムを削除する。
+ * あふwで削除をするときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szItemPath    削除するアイテムのフルパス。
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiDelete(HAFX handle, LPCWSTR szItemPath)
+{
+	lpPluginData pdata = (lpPluginData)handle;
+	if (pdata == NULL) {
+		return 0;
+	}
+
+	int ProcessID;
+	wchar_t ProcessName[1024];
+	swscanf(szItemPath, L"%d %s", &ProcessID, ProcessName);
+
+	HANDLE hProc = OpenProcess(PROCESS_TERMINATE, FALSE, ProcessID);
+	if (hProc == NULL) {
+		return 0;
+	}
+
+	if (!TerminateProcess(hProc, 0)) {
+		return 0;
+	}
+
+	CloseHandle( hProc );
+
+	AfxExec(DISPATCH_METHOD, NULL, pAfxApp, L"&RELOAD", 0);
+
+	return 1;
+}
+
+//------------------------------ 以下ポストローンチ --------------------------------
+
+/**
+ * あふw側からプラグインにアイテムをコピーする。
+ * あふwでコピー処理をするときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szFromItem    コピー元のアイテム名。
+ * @param[in]  szToPath      コピー先のフォルダ。
+ * @param[in]  lpPrgRoutine  コールバック関数(CopyFileExと同様)
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiCopyFrom(HAFX handle, LPCWSTR szFromItem, LPCWSTR szToPath, LPPROGRESS_ROUTINE lpPrgRoutine)
+{
+	return 0;
+}
+
+/**
+ * アイテムを移動する。
+ * あふwで移動処理をするときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szFromPath    移動元のフルパス。
+ * @param[in]  szToPath      移動先のフルパス。
+ * @param[in]  direction     ApiDirection_A2Pはあふwからプラグイン側に移動する場合。
+ *                           ApiDirection_P2Aはプラグイン側からあふwに移動する場合。
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiMove(HAFX handle, LPCWSTR szFromPath, LPCWSTR szToPath, ApiDirection direction)
+{
+	return 0;
+}
+
+/**
+ * アイテムをリネームする。
+ * あふwでリネームをするときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szFromPath    リネーム前のフルパス。
+ * @param[in]  szToName      リネーム後のファイル名。
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiRename(HAFX handle, LPCWSTR szFromPath, LPCWSTR szToName)
+{
+	return 0;
+}
+
+/**
+ * ディレクトリを作成する。
+ * あふwでディレクトリを作成をするときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szItemPath    作成するディレクトリのフルパス。
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiCreateDirectory(HAFX handle, LPCWSTR szItemPath)
+{
+	return 0;
+}
+
+/**
+ * ディレクトリを削除する。
+ * あふwでディレクトリを削除をするときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szItemPath    削除するディレクトリのフルパス。
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiRemoveDirectory(HAFX handle, LPCWSTR szItemPath)
+{
+	return 0;
+}
+
+/**
+ * コンテキストメニュー表示
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  hWnd          あふwのウィンドウハンドル。
+ * @param[in]  x             メニュー表示X座標。
+ * @param[in]  y             メニュー表示Y座標。
+ * @param[in]  szItemPath    アイテムのフルパス。
+ * @retval     2             あふw側に処理を任せる。（ApiCopyでテンポラリにコピーしてからコンテキストメニュー?)
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiShowContextMenu(HAFX handle, const HWND hWnd, DWORD x, DWORD y, LPCWSTR szItemPath)
+{
+	return 0;
+}
+
---
+++ afxtask/afxwapi.h
@@ -0,0 +1,268 @@
+#ifndef __AFXWAPI__
+#define __AFXWAPI__
+/**
+ * [変更履歴]
+ * #001 256→MAXPATH
+ * #002 delete dummy
+ * #003 windowId BYTE→LONG
+ * #004 szWildName削除 (あふw側でやってもらう)
+ * #005 初期相対ディレクトリとタイトル指定を追加
+ * #006 初版のため省略
+ * #007 ApiInternalCopy()を追加
+ * #008 ApiInternalCopy -> ApiIntCopyTo
+ */
+
+#include "windows.h"
+
+#define API_MAX_PLUGIN_NAME_LENGTH	63
+#define API_MAX_AUTHOR_NAME_LENGTH	31
+#define API_MAX_PATH_LENGTH			MAX_PATH // #001 256→MAXPATH
+#define AFXW_WINDOW_ID_SRC			0
+#define AFXW_WINDOW_ID_DST			1
+
+#define MAKE_VERSION(major, minor, revision, build)	\
+		( (major    & 0xFF) << 24) | ( (minor & 0xFF) << 16) | \
+		( (revision & 0xFF) << 8 ) | ( (build & 0xFF)      )
+
+#define APISUCCESS(ret)				(ret == 1)
+#define APIFAILE(ret)				(ret == 0)
+
+#pragma pack(1)
+
+typedef HGLOBAL HAFX;
+
+/**
+ * あふw側から渡される情報
+ */
+typedef struct {
+	HWND		hWnd;		// あふwのウィンドウハンドル
+	DWORD		windowId;	// AFXW_WINDOW_ID_* #003
+	LPVOID		Reserv[8];	// #006
+} AfxwInfo, *lpAfxwInfo;
+
+/**
+ * プラグイン情報
+ */
+typedef struct {
+	WCHAR		szPluginName[API_MAX_PLUGIN_NAME_LENGTH + 1];
+	WCHAR		szAuthorName[API_MAX_AUTHOR_NAME_LENGTH + 1];
+	DWORD		dwVersion;
+	DWORD		dwReserv1;
+	DWORD		dwReserv2;
+	DWORD		dwReserv3;
+	DWORD		dwReserv4;
+} ApiInfo, *lpApiInfo;
+
+/**
+ * ApiOpenで返す情報
+ * #005 ApiFindFirst()のszDirPathには、「基準ディレクトリ」＋「初期相対ディレクトリ」が
+ * #005 渡される。「初期相対ディレクトリ」が「ルート（/)」より上に行くとプラグインを抜ける。
+ */
+typedef struct {
+	WCHAR		szBaseDir[MAX_PATH];    // 基準パス
+	WCHAR		szInitRelDir[MAX_PATH]; // 相対パス #005
+	WCHAR		szTitle[64];            // タイトル #005
+	DWORD		dwReserv1;
+	DWORD		dwReserv2;
+	DWORD		dwReserv3;
+	DWORD		dwReserv4;
+} ApiOpenInfo, *lpApiOpenInfo;
+
+/**
+ * アイテム情報
+ */
+typedef struct {
+	WCHAR		szItemName[API_MAX_PATH_LENGTH];
+	ULONGLONG	ullItemSize;
+	FILETIME 	ullTimestamp;
+	DWORD		dwAttr;			// FindFirstFileやGetFileAttributesと同値(FILE_ATTRIBUTE_*)
+	DWORD		dwReserv1;
+	DWORD		dwReserv2;
+	DWORD		dwReserv3;
+	DWORD		dwReserv4;
+} ApiItemInfo, *lpApiItemInfo;
+
+/**
+ * コピー・移動の方向
+ */
+typedef enum {
+	ApiDirection_A2P,		// あふwからプラグインへ
+	ApiDirection_P2A,		// プラグインからあふwへ
+} ApiDirection;
+
+/**
+ * プラグイン情報を取得する。
+ * @param[out] pluginInfo    プラグイン情報。
+ */
+void WINAPI ApiGetPluginInfo(lpApiInfo pluginInfo);
+
+/**
+ * プラグインオープンする。
+ * @param[in]  szCommandLine プラグイン側に渡すコマンドライン。
+ * @param[in]  afxwInfo      あふwの情報。
+ * @param[out] openInfo      オープン情報。
+ * @retval     0以外         プラグインで自由に使えるハンドル。その他APIのhandleとして使用する。
+ * @retval     NULL(0)       オープン失敗。
+ */
+HAFX WINAPI ApiOpen(LPCWSTR szCommandLine, const lpAfxwInfo afxwInfo, lpApiOpenInfo openInfo);
+
+/**
+ * プラグインクローズする。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiClose(HAFX handle);
+
+/**
+ * szDirPathで指定された仮想ディレクトリパスの先頭アイテムを取得する。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szDirPath     取得したいディレクトリフルパス。
+ * @param[in]  szWildName    アイテムのワイルドカード指定。
+ * @param[out] lpItemInfo    先頭アイテムの情報。
+ * @retval     1             アイテムを見つけた場合
+ * @retval     0             エラー
+ * @retval     -1            検索終了
+ */
+int  WINAPI ApiFindFirst(HAFX handle, LPCWSTR szDirPath, /*LPCWSTR szWildName, #004 delete */ lpApiItemInfo lpItemInfo);
+
+/**
+ * ApiFindFirstで取得したアイテム以降のアイテムを取得する。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[out] lpItemInfo    先頭アイテムの情報。
+ * @retval     1             アイテムを見つけた場合
+ * @retval     0             エラー
+ * @retval     -1            検索終了
+ */
+int  WINAPI ApiFindNext(HAFX handle, lpApiItemInfo lpItemInfo);
+
+/**
+ * プラグインからあふw側にアイテムをコピーする。
+ * あふwでコピー処理をするときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szFromItem    コピー元のアイテム名。
+ * @param[in]  szToPath      コピー先のフォルダ。
+ * @param[in]  lpPrgRoutine  コールバック関数(CopyFileExと同様)
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiCopyTo(HAFX handle, LPCWSTR szFromItem, LPCWSTR szToPath, LPPROGRESS_ROUTINE lpPrgRoutine);
+
+/**
+ * あふwが内部的に利用するコピー処理。
+ * 主にプラグイン内のファイルを開く場合に、AFXWTMP以下にコピーするために使われる。
+ * ただし、実際にコピーするかどうかはプラグイン次第で、あふwはszOutputPathで指定されたファイルを開く。
+ *
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szFromItem    プラグイン内のアイテム名
+ * @param[in]  szToPath      コピー先のフォルダ(c:\Program Files\AFXWTMP.0\hoge.txt等が指定される)
+ * @param[out] szOutputPath  プラグインが実際にコピーしたファイルパス
+ *                           szToPathにコピーする場合はszToPathをszOutputPathにコピーしてあふwに返す。
+ *                           szToPathにコピーしない場合、あふwに開いてほしいファイルのパスを指定する。
+ * @param[in]  dwOutPathSize  szOutputPathのバッファサイズ
+ * @param[in]  lpPrgRoutine  コールバック関数(CopyFileExと同様)
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiIntCopyTo(HAFX handle, LPCWSTR szFromItem, LPCWSTR szToPath, LPWSTR szOutputPath, DWORD dwOutPathSize, LPPROGRESS_ROUTINE lpPrgRoutine);
+
+/**
+ * アイテムを削除する。
+ * あふwで削除をするときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szItemPath    削除するアイテムのフルパス。
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiDelete(HAFX handle, LPCWSTR szItemPath);
+
+
+
+
+
+
+//------------------------------ 以下ポストローンチ --------------------------------
+
+/**
+ * あふw側からプラグインにアイテムをコピーする。
+ * あふwでコピー処理をするときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szFromItem    コピー元のアイテム名。
+ * @param[in]  szToPath      コピー先のフォルダ。
+ * @param[in]  lpPrgRoutine  コールバック関数(CopyFileExと同様)
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiCopyFrom(HAFX handle, LPCWSTR szFromItem, LPCWSTR szToPath, LPPROGRESS_ROUTINE lpPrgRoutine);
+
+/**
+ * アイテムを拡張子判別実行する。
+ * あふwでENTERやSHIFT-ENTERを押したときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szItemPath    アイテムのフルパス。
+ * @retval     2             あふw側に処理を任せる。（ApiCopyでテンポラリにコピーしてから実行)
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiExecute(HAFX handle, LPCWSTR szItemPath);
+
+/**
+ * アイテムを移動する。
+ * あふwで移動処理をするときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szFromPath    移動元のフルパス。
+ * @param[in]  szToPath      移動先のフルパス。
+ * @param[in]  direction     ApiDirection_A2Pはあふwからプラグイン側に移動する場合。
+ *                           ApiDirection_P2Aはプラグイン側からあふwに移動する場合。
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiMove(HAFX handle, LPCWSTR szFromPath, LPCWSTR szToPath, ApiDirection direction);
+
+/**
+ * アイテムをリネームする。
+ * あふwでリネームをするときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szFromPath    リネーム前のフルパス。
+ * @param[in]  szToName      リネーム後のファイル名。
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiRename(HAFX handle, LPCWSTR szFromPath, LPCWSTR szToName);
+
+/**
+ * ディレクトリを作成する。
+ * あふwでディレクトリを作成をするときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szItemPath    作成するディレクトリのフルパス。
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiCreateDirectory(HAFX handle, LPCWSTR szItemPath);
+
+/**
+ * ディレクトリを削除する。
+ * あふwでディレクトリを削除をするときに呼び出される。
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  szItemPath    削除するディレクトリのフルパス。
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiRemoveDirectory(HAFX handle, LPCWSTR szItemPath);
+
+/**
+ * コンテキストメニュー表示
+ * @param[in]  handle        ApiOpenで開いたハンドル。
+ * @param[in]  hWnd          あふwのウィンドウハンドル。
+ * @param[in]  x             メニュー表示X座標。
+ * @param[in]  y             メニュー表示Y座標。
+ * @param[in]  szItemPath    アイテムのフルパス。
+ * @retval     2             あふw側に処理を任せる。（ApiCopyでテンポラリにコピーしてからコンテキストメニュー?)
+ * @retval     1             成功
+ * @retval     0             エラー
+ */
+int  WINAPI ApiShowContextMenu(HAFX handle, const HWND hWnd, DWORD x, DWORD y, LPCWSTR szItemPath);
+
+
+#endif	/* __AFXWAPI__ */
+
